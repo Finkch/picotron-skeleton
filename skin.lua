8c85fd0023cef735cbc1017a99d992fe691e5ef3
Finkch <skyler@thealdersons.org>
2024-07-25 11:51:23 -0700

    Skin now allows regular sprite draws

--- skin.lua
+++ skin.lua
@@ -10,15 +10,21 @@ Skin = {}
 Skin.__index = Skin
 Skin.__type = "skin"
 
-function Skin:new(sprite, offset, tsize, toffset)
-    sprite = get_spr(sprite)
-    
+function Skin:new(sprite_num, ismap, offset, tsize, toffset)
+    if (ismap == nil)   ismap = true
     if (not offset)     offset = Vec:new()
-    if (not toffset)    toffset = Vec:new()
-    if (not tsize)      tsize = Vec:new(sprite:width() - 1, sprite:height() - 1)
+
+    local sprite = nil
+    if (ismap) then
+        sprite = get_spr(sprite_num)
+        if (not toffset)    toffset = Vec:new()
+        if (not tsize)      tsize = Vec:new(sprite:width() - 1, sprite:height() - 1)
+    end
     
     local s = {
-        sprite = sprite,    -- sprite id of the texture element
+        sprite = sprite,    -- sprite bitmap for the texture element
+        sn = sprite_num,    -- sprite number
+        ismap = ismap,      -- whether to draw map or just a sprite
         tsize = tsize,      -- texture coordinates of skin map
         offset = offset,    -- offset to draw position
         toffset = toffset,  -- offset to texture position
@@ -31,20 +37,31 @@ end
 
 function Skin:draw(bone, offset)
 
-    -- grabs the bone's range
-    local s, e = bone:span(self.offset + offset)
+    if (self.ismap) then
 
-    -- grabs texture element range
-    local ts, te = self.toffset, self.toffset + self.tsize
+        -- grabs the bone's range
+        local s, e = bone:span(self.offset + offset)
 
-    -- draws a textured line
-    tline3d(
-        self.sprite,    -- sprite data
-        s.x, s.y,       -- x0, y0
-        e.x, e.y,       -- x1, y1
-        ts.x, ts.y,     -- texture x0, y0
-        te.x, te.y      -- texture x1, y1
-    )
+        -- grabs texture element range
+        local ts, te = self.toffset, self.toffset + self.tsize
+
+        -- draws a textured line
+        tline3d(
+            self.sprite,    -- sprite data
+            s.x, s.y,       -- x0, y0
+            e.x, e.y,       -- x1, y1
+            ts.x, ts.y,     -- texture x0, y0
+            te.x, te.y      -- texture x1, y1
+        )
+    else
+        local pos = self.offset + offset
+
+        spr(
+            self.sn,
+            pos.x,
+            pos.y
+        )
+    end
 end
 
 
